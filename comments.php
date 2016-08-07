785327f549be2fc016a894b504b2fbf7c4c25e6a
Sam Hermes <samhermes@me.com>
2016-08-07 14:12:22 -0500

    Begin styling single post

--- comments.php
+++ comments.php
@@ -24,15 +24,6 @@ if ( post_password_required() ) {
 
 	<?php
 	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'harper' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			?>
-		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,7 +25,7 @@ function harper_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'harper' ),
+		esc_html_x( '%s', 'post date', 'harper' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
