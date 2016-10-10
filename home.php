493d7a7c6d802cf1e242b49eb90d52beaed123af
Sam Hermes <sam.hermes@wustl.edu>
2016-10-10 14:59:03 -0500

    Start work on homepage template

---
+++ home.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying an overview of posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Harper
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		if ( have_posts() ) :
+
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', get_post_format() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif; ?>
+		
+		</main>
+	</div>
+
+<?php
+get_footer();
