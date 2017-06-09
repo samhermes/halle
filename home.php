2ca2783cf731f9f9e82bf4e56b3ff351e19eca8d
Sam Hermes <samhermes@me.com>
2017-06-08 19:52:25 -0500

    Intro posts styles

--- home.php
+++ home.php
@@ -53,7 +53,7 @@ get_header(); ?>
 
 			<?php
 			$args = array(
-				'offset' => 4,
+				'posts_per_page' => -1,
 			);
 
 			$the_query = new WP_Query( $args );
@@ -62,7 +62,7 @@ get_header(); ?>
 				while ( $the_query->have_posts() ) {
 					$the_query->the_post();
 
-					get_template_part( 'template-parts/content', 'archive' );
+					get_template_part( 'template-parts/content', 'home-feed' );
 				}
 			
 			wp_reset_postdata();
