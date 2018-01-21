0688ba302d25b272f40c34c0903e4827781a5179
Sam Hermes <sam.hermes@wustl.edu>
2018-01-21 11:02:29 -0600

    Add post thumbnail conditional to featured story partial

--- template-parts/content-featured.php
+++ template-parts/content-featured.php
@@ -13,7 +13,11 @@
 	<header class="entry-header">
 		<?php
 		echo '<div class="image-wrap"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">';
-		the_post_thumbnail( 'halle-post-3x2' );
+		
+		if ( has_post_thumbnail() ) {
+			the_post_thumbnail( 'halle-post-3x2' );
+		}
+		
 		echo '</a></div>';
 		?>
 
