eaa50656ae0ce4b53f4b9fa60bc26f6a88afa2cc
Sam Hermes <sam.hermes@wustl.edu>
2018-03-14 11:38:18 -0500

    Remove unused byline variable

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -29,11 +29,6 @@ function halle_posted_on() {
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'halle' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
 	echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 }
